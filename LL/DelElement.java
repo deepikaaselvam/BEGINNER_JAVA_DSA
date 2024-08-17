724b20a2a593cf9007d361dc134b6c1c1fddaf27
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-08-18 00:10:55 +0530

    Update the message

--- LL/DelElement.java
+++ LL/DelElement.java
@@ -4,23 +4,22 @@ public class DelElement {
     {
         Node1 current=head;
         Node1 prev=null;
-        while(current!=null&& current.data!=ele)
-        {
-            prev=current;
+        // while(current!=null&& current.data!=ele)
+        // {
+        //     prev=current;
+        //     current=current.next;
+        // }
+       while(current!=null)
+       {
+            if(current.data != ele)
+            {
+                System.out.print(current.data+" ");
+                prev=current;
+            }
             current=current.next;
         }
-       
-        if(current.data == ele)
-        {
-           prev.next=prev.next.next;
-        }
-    
-        
-            System.out.print(current.data+" ");
-            prev=current;
-            current=current.next;
-        
-    }
+}
+
     public static void main(String[] args) {
         Node1 head=new Node1(23);
         head.next=new Node1(24);
