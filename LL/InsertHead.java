ae7c44e30fbc274ca48ef7c9fb504dc683d3fb00
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-08-24 10:07:48 +0530

    InsertHead will be addded

--- LL/InsertHead.java
+++ LL/InsertHead.java
@@ -7,6 +7,7 @@ public class InsertHead {
         newNode.next=head;
         System.out.println();
        System.out.println("\nAfter Inserting the Node\n");
+    
         while(newNode!=null)
         {
             System.out.print(newNode.data+"  ");
@@ -14,11 +15,23 @@ public class InsertHead {
         }
         return newNode;
     }
+    public static Node1 insertFirst(Node1 head,int val)
+    {
+        Node1 newNode1=new Node1(val,head);
+        System.out.println("After insert the node will be");
+        while(newNode1!=null)
+        {
+            System.out.print(newNode1.data+"  ");
+            newNode1=newNode1.next;
+        }
+        return newNode1;
+
+    }
     public static void main(String[] args) {
         Node1 head=new Node1(13);
         head.next=new Node1(34);
         head.next.next=new Node1(45);
-
+       
         System.out.println("\nBefore Insert the Head Node"+"\n");
         Node1 current=head;
           while(current!=null)
@@ -26,10 +39,11 @@ public class InsertHead {
             System.out.print(current.data+" ");
             current=current.next;
          }
+         System.out.println();
+         insertFirst(head,100);
 
-
-
-        insertFirst(head);
+        // insertFirst(head);
+       
        
        
     }
