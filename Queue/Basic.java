e097b210f38f51a12a57a79e9e701f767f91852c
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-02 19:33:41 +0530

    Queue Folder is added

---
+++ Queue/Basic.java
@@ -0,0 +1,18 @@
+package Queue;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Basic {
+    public static void main(String[] args) {
+        Queue<Integer>queue=new LinkedList<>();
+        queue.add(10);
+        queue.offer(20);
+        queue.add(30);
+        System.out.println(queue);//[10,20,30]
+        System.out.println(queue.peek());//10
+        System.out.println(queue.poll());//10
+
+        
+    }
+    
+}
