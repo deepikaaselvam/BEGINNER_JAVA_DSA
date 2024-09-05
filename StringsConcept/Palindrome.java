07a96c84c8c9b23e60412f2c1f6045757dcd0dc4
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-05 19:01:21 +0530

    Add palindrome file

---
+++ StringsConcept/Palindrome.java
@@ -0,0 +1,30 @@
+package StringsConcept;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String str="amama";
+        int n=str.length();
+        int i=0;
+        int j=n-1;
+        boolean Palindrome=true;
+        while(i<j)
+        {
+            
+            if(str.charAt(i)!=str.charAt(j))
+            {
+                Palindrome=false;
+               
+            }
+            i++;
+            j--;
+        }
+        if(Palindrome==true)
+        {
+            System.out.println("String is the Palindrome");
+        }
+        else{
+            System.out.println("String is not the Palindrome");
+        }
+    }
+    
+}
