a2a63c7dc239b7c2d1fb1eedcea48f2954f5e025
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-06 20:17:51 +0530

    Add these Folder

--- LL/RemDup.java
+++ LL/RemDup.java
@@ -1,18 +1,50 @@
 package LL;
 import java.util.HashSet;
-public class RemDup {
-    public static HashSet<Object> removeDuplicatesInLL(Node head)
+class Node{
+    int data;
+    Node next;
+    Node(int data)
     {
+        this.data=data;
+        this.next=null;
+    }
+    Node(int data,Node next)
+    {
+        this.data=data;
+        this.next=next;
+    }
+}
+public class RemDup {
+    public static Node removeDuplicatesInLL(Node head)
+    {
+        HashSet<Integer>set=new HashSet<>();
+        Node prev=null;
         Node current=head;
-        HashSet<Object>set=new HashSet<>();
         while(current!=null)
         {
-            set.add(current.data);
+            if(set.contains(current.data))
+            {
+                prev.next=current.next;
+            }
+            else{
+                set.add(current.data);
+               
+            }
+            prev=current;
             current=current.next;
         }
-        return set;
+        return head;
 
 
+    }
+    public static void printList(Node head)
+    {
+        Node curr=head;
+        while(curr!=null)
+        {
+            System.out.print(curr.data+" ");
+            curr=curr.next;
+        }
     }
     public static void main(String[] args) {
         //Duplicate Node will be created!!!
@@ -20,8 +52,10 @@ public class RemDup {
         head.next=new Node(1);
         head.next.next=new Node(2);
         head.next.next.next=new Node(2);
-        head.next.next.next.next=new Node(5);
-        System.out.println(removeDuplicatesInLL(head));
+        head.next.next.next.next=new Node(3);
+        // head.next.next.next.next.next=new Node(5);
+        head=removeDuplicatesInLL(head);
+        printList(head);
 
     }
     
