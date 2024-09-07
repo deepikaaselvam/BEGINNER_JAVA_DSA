fa2a05db5d251028290df68758340a97d4b7d753
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-07 22:21:40 +0530

    Add these Folders

--- Stack/InfixToPrefix.java
+++ Stack/InfixToPrefix.java
@@ -1,10 +1,24 @@
 package Stack;
+import java.util.Stack;
 import java.lang.StringBuilder;
 public class InfixToPrefix {
     public static void main(String[] args) {
         String s="(A+B)*C-D+E";
         StringBuilder sb=new StringBuilder(s);
         StringBuilder stringg=sb.reverse();
-        System.out.println(stringg);
+        String answer="";
+        int i=0;
+        int n=stringg.length();
+        Stack<Character>stk=new Stack<>();
+        while(i<n)
+        {
+            if((i>='A'&&i<='Z') ||
+            (i>='a'&&i<='z')||
+            (i>='0'&&i<='9'))
+            {
+                answer=answer+stringg.charAt(i);
+            }
+        }
+        // System.out.println(stringg);
     }
 }
---
+++ Stack/PrefixToInfix.java
@@ -0,0 +1,39 @@
+package Stack;
+import java.util.Stack;
+import java.lang.String;
+// import java.lang.Character;
+public class PrefixToInfix {
+    public static String prefixToInfixOrder(String s){
+        Stack<String>st=new Stack<>();
+        int n=s.length()-1;
+        String answer="";
+        int i=n;
+        while(i>=0)
+        {
+            char c=s.charAt(i);
+            if((s.charAt(i)>='A'&&s.charAt(i)<='Z')||(s.charAt(i)>='a'&&s.charAt(i)<='z')||
+            (s.charAt(i)>='0'&& s.charAt(i)<='9'))
+            {
+                answer=answer+s.charAt(i);
+                st.push(Character.toString(c));
+            }
+            else{
+                String peek1=st.peek();
+                st.pop();
+                String peek2=st.peek();
+                st.pop();
+                answer="("+peek1+s.charAt(i)+peek2+")";
+                st.push(answer);
+
+            }
+            i--;
+        }
+        return st.pop();
+
+    }
+    public static void main(String[] args) {
+        String oldString="*+PQ-MN";
+        System.out.println(prefixToInfixOrder(oldString));
+    }
+    
+}
