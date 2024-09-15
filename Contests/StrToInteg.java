9e11878aef3dcafed3e2441394b4e1eb7cf3a900
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-15 09:24:11 +0530

    InBuilt String to Integer COnversion

---
+++ Contests/StrToInteg.java
@@ -0,0 +1,12 @@
+package Contests;
+public class StrToInteg {
+    public static void main(String[] args) {
+        String number="123";
+        int num1=Integer.parseInt(number);
+        System.out.println(num1);
+        
+    }
+    
+    
+    
+}
