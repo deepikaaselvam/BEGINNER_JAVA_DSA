0ad8156a5de93bfa5c123ea3882a173bcbd79165
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-21 12:44:48 +0530

    Searching the element using O(log n) time complexity

--- Searching/BSearch.java
+++ Searching/BSearch.java
@@ -3,15 +3,14 @@ public class BSearch {
     public static int binarySearch(int[]array,int n,int target)
     {
         int l=0,r=array.length-1;
-        int ans=0;
-        int index=0;
+      
+        int index=-1;
         while(l<=r)
         {
-            int mid=(l+r)/2;
+            int mid=(l+(r-l)/2);
             if(array[mid]==target)
             {
                  index=mid;
-                ans=1;
                 break;
             }
             else if(array[mid]<target)
@@ -22,16 +21,16 @@ public class BSearch {
                 r=mid-1;
             }
         }
-        if(ans==0)
+        if(index==-1)
         {
             return -1;
         }
         return index;
     }
     public static void main(String[] args) {
-        int[]arr={3,4,6,7,9,12,6,17};
-        int n=arr.length;int target=12;
-        System.out.println(binarySearch(arr,n,target));
+        int[]arr={1,2,3,4,5,6};
+        int n=arr.length;int target=5;
+        System.out.println("index : "+binarySearch(arr,n,target));
         
     }
 }
