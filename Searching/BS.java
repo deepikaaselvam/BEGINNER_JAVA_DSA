1c632d7574a9f224afb5c85234987a3e297a575f
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-21 12:53:32 +0530

    Iterative BSearch using O(logn) is implemented

---
+++ Searching/BS.java
@@ -0,0 +1,36 @@
+package Searching;
+public class BS {
+    public static int binarySearch(int[]array,int n,int target)
+    {
+        int l=0,r=array.length-1;
+      
+        int index=-1;
+        while(l<=r)
+        {
+            int mid=(l+(r-l)/2);
+            if(array[mid]==target)
+            {
+                 index=mid;
+                break;
+            }
+            else if(array[mid]<target)
+            {
+                l=mid+1;
+            }
+            else{
+                r=mid-1;
+            }
+        }
+        if(index==-1)
+        {
+            return -1;
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5,6};
+        int n=arr.length;int target=5;
+        System.out.println("index : "+binarySearch(arr,n,target));
+        
+    }
+}
