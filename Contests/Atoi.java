67c5c24084a115b9768fa17b874117af9f875888
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-24 20:14:21 +0530

    Good Atoi Implemenetattion

--- Contests/Atoi.java
+++ Contests/Atoi.java
@@ -1,15 +1,25 @@
 package Contests;
 public class Atoi {
     public static void main(String[] args) {
-        String number="123";
+        String number="-123";
         char[]Chararray=number.toCharArray();
         int result=0;
+        int sign=1;
         for(int i=0;i<Chararray.length;i++)
         {
+            if(Chararray[i]=='-')
+            {
+                sign=-1;
+                i++;
+            }
+            else if(Chararray[i]=='+')
+            {
+                i++;
+            }
             int ans=Chararray[i]-'0';
             result=result*10+ans;
         }
-        System.out.println(result);
+        System.out.println(result*sign);
 
     }
     
