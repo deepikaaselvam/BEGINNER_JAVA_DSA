ac5a0125917dfc813d3726881c26d8cf6bf40d07
deepikaaselvam <deepikaselvam.16@gmail.com>
2024-09-26 19:30:54 +0530

    Inverse the TRiangle

--- Patterns/Inverse.java
+++ Patterns/Inverse.java
@@ -3,6 +3,7 @@ package Patterns;
 public class Inverse {
     public static void main(String[] args) {
         int n=5;
+        char Alpha='a';
         for(int i=1;i<=n;i++)
         {
            for(int j=1;j<=n-i;j++)
@@ -11,8 +12,9 @@ public class Inverse {
            }
            for(int j=n-i+1;j<=n;j++)
            {
-                System.out.print(" *");
+                System.out.print(Alpha--+" ");
            }
+           Alpha +=(i+1);
             System.out.println();
         }
     }
